bcc9a545a2342a63954cf85c977dedb77e511ee1
charles <charles.agostinelli26@gmail.com>
2024-01-24 15:21:56 +0100

    1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {useState} from "react";
+import axios from "axios";
 
 const Tab1: React.FC = () => {
+
+    const [joke, setJoke]= useState()
+
+    function getAJokes(){
+
+        axios.get("https://api.chucknorris.io/jokes/random")
+            .then((response)=>{
+                setJoke(response.data.value)
+                console.log(response.data.value)
+
+            })
+
+    }
+
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+      <IonPage>
+          <IonHeader>
+              <IonToolbar>
+                  <IonTitle>joke</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent fullscreen>
+              <p>{joke}</p>
+
+              <IonHeader collapse="condense">
+                  <IonToolbar>
+                      <IonTitle size="large">Tab 1</IonTitle>
+                  </IonToolbar>
+              </IonHeader>
+              <ExploreContainer name="Tab 1 page"/>
+              <IonButton onClick={getAJokes} className="btn btn-success">Change jokes</IonButton>
+
+          </IonContent>
+      </IonPage>
+  )
+      ;
 };
 
 export default Tab1;
